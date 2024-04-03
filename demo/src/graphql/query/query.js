11b2966f2a33814a775b58d1f4c0b903b626b25a
Andrew <apwicker@gmail.com>
2024-04-03 15:08:38 -0700

    updated display label for root query field to match schema ('company' -> 'companies')

--- demo/src/graphql/query/query.js
+++ demo/src/graphql/query/query.js
@@ -1,7 +1,7 @@
 import React from 'react-redux';
 import { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { submitQuery, fetchClient, formatQuery, fetchServer} from '../../reducers/counterSlice';
+import { submitQuery, fetchClient, formatQuery, fetchServer } from '../../reducers/counterSlice';
 import './query.css';
 import Fields from '../fields/fields';
 
@@ -9,9 +9,9 @@ function Query() {
   const dispatch = useDispatch();
   const fields = useSelector((state) => state.counter.fields);
   const formattedQuery = useSelector((state) => state.counter.formattedQuery);
-  const queryType = useSelector((state)=>state.counter.queryType)
-  const queryID = useSelector((state)=>state.counter.queryID)
-  const enviornment = useSelector((state)=> state.counter.enviornment)
+  const queryType = useSelector((state) => state.counter.queryType);
+  const queryID = useSelector((state) => state.counter.queryID);
+  const enviornment = useSelector((state) => state.counter.enviornment);
   //const randomQueryID = Math.floor(Math.random() * 5)
   const queryIDDescription = `(id: Company${1})`;
   const queryMutationDescription = `{ addCompany(input: {
@@ -23,8 +23,6 @@ function Query() {
   const fieldnames = ['company', 'city', 'state'];
   const departmentnames = ['departmentName'];
   const productnames = ['productName', 'productDescription', 'price'];
-  
-
 
   // renders with dependencies - formattedQuery initial empty string
   useEffect(() => {
@@ -44,57 +42,64 @@ function Query() {
   };
 
   return (
-      <div className='wholecontainer'>
+    <div className='wholecontainer'>
+      <div className='queryBox'>
+        <Fields />
 
-          <div className='queryBox'>
-            <Fields />
-            
-            <div className='graphql-query'>
-              <div className='query'>
-              {queryType === '' && <> {'[select your operation type]'}</>}
-              {queryType === 'query' && queryID === 'no id' && <> {`${queryType} {`}</>}
-                {queryType === 'query' && queryID === 'id' && <> {`${queryType} ${queryIDDescription} {`}</>}
-                {queryType === 'mutation' && <> {`${queryType} ${queryMutationDescription} {`}</>}
-                <div className='indent'>
-                  company {'{'}
-                  <div className='indent'>
-                    {fields.map((item, index) => {
-                      if (fieldnames.includes(item)) {
-                        return (
-                          <div key={index} className='field'>
-                            {item}
-                          </div>
-                        );
-                      } else return null;
-                    })}
-                    <div>
-                      department {'{'}
-                      {fields.map((item, index) => {
-                        if (departmentnames.includes(item)) {
-                          return (
-                            <div key={index} className='field'>
-                              {item}
-                            </div>
-                          );
-                        } else return null;
-                      })}
-                      
-                    </div>
-                    {'}'}
-                  </div>
-                  {'}'}
+        <div className='graphql-query'>
+          <div className='query'>
+            {queryType === '' && <> {'[select your operation type]'}</>}
+            {queryType === 'query' && queryID === 'no id' && <> {`${queryType} {`}</>}
+            {queryType === 'query' && queryID === 'id' && <> {`${queryType} ${queryIDDescription} {`}</>}
+            {queryType === 'mutation' && <> {`${queryType} ${queryMutationDescription} {`}</>}
+            <div className='indent'>
+              companies {'{'}
+              <div className='indent'>
+                {fields.map((item, index) => {
+                  if (fieldnames.includes(item)) {
+                    return (
+                      <div
+                        key={index}
+                        className='field'
+                      >
+                        {item}
+                      </div>
+                    );
+                  } else return null;
+                })}
+                <div>
+                  department {'{'}
+                  {fields.map((item, index) => {
+                    if (departmentnames.includes(item)) {
+                      return (
+                        <div
+                          key={index}
+                          className='field'
+                        >
+                          {item}
+                        </div>
+                      );
+                    } else return null;
+                  })}
                 </div>
                 {'}'}
               </div>
-              <div className='buttonContainer'>
-                <button type='button' className='queryButton' onClick={(e) => handleBoxClick(e)}>
-                  Submit Query
-                </button>
-              </div>
+              {'}'}
             </div>
+            {'}'}
           </div>
+          <div className='buttonContainer'>
+            <button
+              type='button'
+              className='queryButton'
+              onClick={(e) => handleBoxClick(e)}
+            >
+              Submit Query
+            </button>
+          </div>
+        </div>
       </div>
-    
+    </div>
   );
 }
 
